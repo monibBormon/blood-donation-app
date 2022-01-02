9751f348dbd43be5f22dbfa4883ac21b92fd734d
monibBormon <monibbormon14@gmail.com>
2022-01-02 12:39:11 +0600

    font awesome icon added

--- src/features/FindDonar/FindDonar.js
+++ src/features/FindDonar/FindDonar.js
@@ -3,7 +3,16 @@ import React from 'react';
 const FindDonar = () => {
     return (
         <div>
-            <h2>Find Donar</h2>
+            <div className="container mx-auto py-14">
+                <div className="grid grid-cols-6">
+                    <div className="col-start-2 col-span-4">
+                        <div className="donar-search-box">
+                            <input className='border-2 border-red-300 rounded-full focus:outline-none w-3/4 h-12 pl-5' type="text" placeholder='Search donar' />
+                            <button><i class="fas fa-search"></i> Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
