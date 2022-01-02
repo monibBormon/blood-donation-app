a2136762253182a28906ddf3936f0ce99370f085
monibBormon <monibbormon14@gmail.com>
2022-01-02 12:28:05 +0600

    created find donar page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import FindDonar from './features/FindDonar/FindDonar';
 import Home from './features/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='find-donar' element={<FindDonar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/features/FindDonar/FindDonar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FindDonar = () => {
+    return (
+        <div>
+            <h2>Find Donar</h2>
+        </div>
+    );
+};
+
+export default FindDonar;
